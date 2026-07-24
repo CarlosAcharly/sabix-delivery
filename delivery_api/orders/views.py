9746a5b28d9501eae3baf4a7cf7091f104510fd9
Carlos Hernández <charlyc1502@gmail.com>
2026-07-23 18:56:40 -0600

    fix: importacion

--- delivery_api/orders/views.py
+++ delivery_api/orders/views.py
@@ -9,6 +9,7 @@ from .models import Order, OrderItem, DeliveryTracking
 from django.contrib.auth import get_user_model
 from geopy.distance import geodesic
 from django.db.models import Q
+from django.db import models  # ✅ Agregar esta línea
 
 User = get_user_model()
 from .serializers import (
